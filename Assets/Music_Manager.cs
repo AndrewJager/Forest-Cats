1ae95d53e844f602ee47ae0e5214cd2e7bb2bbbd
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2018-08-24 19:16:10 -0400

    Added music management

---
+++ Assets/Music_Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Manager : MonoBehaviour {
+	public enum MusicState {homeNormal, fight};
+	public MusicState state;
+	public AudioClip [] home_normal;
+	AudioSource player;
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<AudioSource>();
+		PlaySong();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.isPlaying == false){
+			PlaySong();
+		}
+	}
+
+	void PlaySong (){
+		if (state == MusicState.homeNormal){
+			AudioClip song = home_normal[Random.Range(0,home_normal.Length)];
+			player.clip = song;
+			player.loop = false;
+			player.Play();
+			player.pitch = 4;
+		}
+		else if (state == MusicState.fight){
+
+		}
+		else{
+			Debug.Log("music state not defined in code!");
+		}
+	}
+}
