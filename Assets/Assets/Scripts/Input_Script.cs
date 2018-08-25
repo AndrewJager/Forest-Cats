2c8b809b5a8708825269986e4116d9ccd9e2c3b3
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2018-08-25 12:53:14 -0400

    Scene improvement, removed unused script

---
+++ Assets/Assets/Scripts/Input_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Script : MonoBehaviour {
+	private UnityEngine.UI.Text input;
+	public Manager_Script manager;
+	private string value;
+	public int valueToWriteTo;
+
+	// Use this for initialization
+	void Start () {
+		input = GetComponent<UnityEngine.UI.Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (input.text != value){
+			manager.values[valueToWriteTo] = input.text;
+		}
+	}
+}
--- Assets/Assets/Scripts/Manager_Script.cs
+++ Assets/Assets/Scripts/Manager_Script.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Manager_Script : MonoBehaviour {
 	public List <Edge_Script> exits;
+	public int currentScene;
 	public string PlayerName;
+	public List <string> values;
+
+	public void Start (){
+		PlayerName = "Player Name";
+		Scene scene = SceneManager.GetActiveScene();
+		currentScene = scene.buildIndex;
+	}
 	
 	// Update is called once per frame
 	public void Update(){
+		//Debug.Log("update start");
 		for (int i = 0; i < exits.Count; i++){
 			if (exits[i].startNewLevel == true){
 				RunScene(exits[i].newScene);
 			}
 		}
+		
 	}
 
 	void Awake(){
