b0b27de0394e33703a75a7efaaf73cb36bf28866
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2018-08-25 17:23:05 -0400

    Allowed music to change when scene changes

--- Assets/Assets/Scripts/Manager_Script.cs
+++ Assets/Assets/Scripts/Manager_Script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Manager_Script : MonoBehaviour {
+	private Music_Manager music;
 	public List <Edge_Script> exits;
 	public int currentScene;
 	public string PlayerName;
@@ -12,19 +13,26 @@ public class Manager_Script : MonoBehaviour {
 
 	public void Start (){
 		PlayerName = "Player Name";
+		music = GetComponent<Music_Manager>();
 		Scene scene = SceneManager.GetActiveScene();
 		currentScene = scene.buildIndex;
 	}
 	
 	// Update is called once per frame
 	public void Update(){
-		//Debug.Log("update start");
+		Scene scene = SceneManager.GetActiveScene();
+		currentScene = scene.buildIndex;
 		for (int i = 0; i < exits.Count; i++){
 			if (exits[i].startNewLevel == true){
 				RunScene(exits[i].newScene);
 			}
 		}
-		
+		if (currentScene == 0){// Main menu
+			music.state = Music_Manager.MusicState.mainMenu;
+		}
+		else if(currentScene == 1){// Camp
+			music.state = Music_Manager.MusicState.homeNormal;
+		}
 	}
 
 	void Awake(){
--- Assets/Assets/Scripts/Music_Manager.cs
+++ Assets/Assets/Scripts/Music_Manager.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Music_Manager : MonoBehaviour {
 	public bool mute;
-	public enum MusicState {homeNormal, fight};
+	public enum MusicState {homeNormal, mainMenu, fight};
 	public MusicState state;
+	private MusicState playingState;
 	public AudioClip [] home_normal;
+	public AudioClip [] main_menu;
 	AudioSource player;
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<AudioSource>();
+		player.loop = false;
 		PlaySong();
+		playingState = state;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (player.isPlaying == false){
 			PlaySong();
+		}
+		if (playingState != state){
+			PlaySong();
+			playingState = state;
 		}
 	}
 
@@ -26,7 +34,11 @@ public class Music_Manager : MonoBehaviour {
 			if (state == MusicState.homeNormal){
 				AudioClip song = home_normal[Random.Range(0,home_normal.Length)];
 				player.clip = song;
-				player.loop = false;
+				player.Play();
+			}
+			else if (state == MusicState.mainMenu){
+				AudioClip song = main_menu[Random.Range(0, main_menu.Length)];
+				player.clip = song;
 				player.Play();
 			}
 			else if (state == MusicState.fight){
