4eef725bf33020ce44e0132ca67f53407e62004f
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2018-09-09 20:48:44 -0400

    changed .gitignore

--- Assets/Assets/Scripts/Manager_Script.cs
+++ Assets/Assets/Scripts/Manager_Script.cs
@@ -72,7 +72,7 @@ public class Manager_Script : MonoBehaviour {
 
 		otherManagers = GameObject.FindGameObjectsWithTag("Manager");
 		if (otherManagers.Length > 1){
-			Destroy(otherManagers[0]); // THERE CAN ONLY BE ONE!
+			Destroy(otherManagers[0]); // THERE CAN ONLY BE ONE! (is this murder or suicide?)
 		}
 		otherManagers = null;
 
